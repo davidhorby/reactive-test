8b277dac0bb60da311184d2dca13f358e5771834
David Horby <david.horby@springernature.com>
2018-04-13 16:15:05 +0100

    Update tests and test output

--- src/test/kotlin/com/dhorby/reactive/controllers/MainControllerIntegrationTest.kt
+++ src/test/kotlin/com/dhorby/reactive/controllers/MainControllerIntegrationTest.kt
@@ -1,58 +1,35 @@
 package com.dhorby.reactive.controllers
 
-import com.dhorby.reactive.dao.ArticleRepository
 import com.dhorby.reactive.entities.Article
-import com.fasterxml.jackson.databind.ObjectMapper
-import org.junit.Before
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.MockitoAnnotations
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.context.junit4.SpringRunner
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import org.springframework.test.web.servlet.setup.MockMvcBuilders
-import org.springframework.web.context.WebApplicationContext
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.web.reactive.function.BodyInserters
 
 
 @RunWith(SpringRunner::class)
-@WebMvcTest(MainController::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class MainControllerIntegrationTest {
 
-    @MockBean
-    lateinit var repository: ArticleRepository;
-
     @Autowired
-    lateinit var wac: WebApplicationContext;
-
-    lateinit var mvc: MockMvc;
-
-    @Before
-    fun initMocks() {
-        MockitoAnnotations.initMocks(this)
-    }
-
-    @Before
-    fun setup() {
-        mvc = MockMvcBuilders.webAppContextSetup(wac).build()
-    }
+    lateinit var webTestClient: WebTestClient
 
     @Test
     fun `should save an article`() {
 
         val article = Article("1", "Title 1", "Desc 1", "link", "01/01/2015")
-        val mapper = ObjectMapper()
-        val articleJson = mapper.writeValueAsString(article)
-        mvc.perform(post("/")
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .content(articleJson))
-                .andExpect(status().isCreated)
+        webTestClient
+                .post()
+                .uri("/")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .body(BodyInserters.fromObject(article))
+                .exchange()
+                .expectStatus().isCreated;
 
     }
 
